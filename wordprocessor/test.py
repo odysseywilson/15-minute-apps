d5dc0311b468e0dfdbe6f34f25fdfd7012c69e2d
Alexander Baird-Appleton <bairdapa@oregonstate.edu>
2020-02-04 15:43:07 -0800

    added tests for italic and underline buttons

--- wordprocessor/test.py
+++ wordprocessor/test.py
@@ -16,7 +16,7 @@ class AppTest(unittest.TestCase):
 
     def test_bold_button(self):
         # store the state of the bold button
-        bold_before = self.form.bold_action.isChecked()
+        button_state_before = self.form.bold_action.isChecked()
 
         # get the bold button from the form
         bold_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.bold_action)
@@ -25,10 +25,45 @@ class AppTest(unittest.TestCase):
         QTest.mouseClick(bold_button, Qt.LeftButton)
 
         # store the state of the bold button
-        bold_after = self.form.bold_action.isChecked()
+        button_state_after = self.form.bold_action.isChecked()
 
         # assert that the button state is not the same before and after the click
-        self.assertNotEqual(bold_before, bold_after)
+        self.assertNotEqual(button_state_before, button_state_after)
 
+
+    def test_italic_button(self):
+        # store the state of the italic button
+        button_state_before = self.form.italic_action.isChecked()
+
+        # get the italic button from the form
+        italic_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.italic_action)
+
+        # left click the button
+        QTest.mouseClick(italic_button, Qt.LeftButton)
+
+        # store the state of the italic button
+        button_state_after = self.form.italic_action.isChecked()
+
+        # assert that the button state is not the same before and after the click
+        self.assertNotEqual(button_state_before, button_state_after)
+
+
+    def test_underline_button(self):
+        # store the state of the underline button
+        button_state_before = self.form.underline_action.isChecked()
+
+        # get the underline button from the form
+        underline_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.underline_action)
+
+        # left click the button
+        QTest.mouseClick(underline_button, Qt.LeftButton)
+
+        # store the state of the underline button
+        button_state_after = self.form.underline_action.isChecked()
+
+        # assert that the button state is not the same before and after the click
+        self.assertNotEqual(button_state_before, button_state_after)
+    
+    
 if __name__ == '__main__':
     unittest.main()
