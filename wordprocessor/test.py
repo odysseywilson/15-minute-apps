8c2c42e5d294e6e2e456d2ba92e0fb3a02d79871
Alexander Baird-Appleton <bairdapa@oregonstate.edu>
2020-02-04 18:28:00 -0800

    added tests for alignment buttons

--- wordprocessor/test.py
+++ wordprocessor/test.py
@@ -17,53 +17,94 @@ class AppTest(unittest.TestCase):
     def test_bold_button(self):
         # store the state of the bold button
         button_state_before = self.form.bold_action.isChecked()
-
         # get the bold button from the form
         bold_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.bold_action)
-
         # left click the button
         QTest.mouseClick(bold_button, Qt.LeftButton)
-
         # store the state of the bold button
         button_state_after = self.form.bold_action.isChecked()
-
         # assert that the button state is not the same before and after the click
         self.assertNotEqual(button_state_before, button_state_after)
-
 
     def test_italic_button(self):
         # store the state of the italic button
         button_state_before = self.form.italic_action.isChecked()
-
         # get the italic button from the form
         italic_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.italic_action)
-
         # left click the button
         QTest.mouseClick(italic_button, Qt.LeftButton)
-
         # store the state of the italic button
         button_state_after = self.form.italic_action.isChecked()
-
         # assert that the button state is not the same before and after the click
         self.assertNotEqual(button_state_before, button_state_after)
-
 
     def test_underline_button(self):
         # store the state of the underline button
         button_state_before = self.form.underline_action.isChecked()
-
         # get the underline button from the form
         underline_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.underline_action)
-
         # left click the button
         QTest.mouseClick(underline_button, Qt.LeftButton)
-
         # store the state of the underline button
         button_state_after = self.form.underline_action.isChecked()
-
         # assert that the button state is not the same before and after the click
         self.assertNotEqual(button_state_before, button_state_after)
-    
-    
+
+    def test_alignl_button(self):
+        # get the alignment buttons from the form
+        alignl_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignl_action)
+        alignc_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignc_action)
+        alignr_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignr_action)
+        alignj_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignj_action)
+        # left click the alignl button
+        QTest.mouseClick(alignl_button, Qt.LeftButton)
+        # assert that only the correct setting is enabled
+        self.assertEqual(self.form.alignl_action.isChecked(), True)
+        self.assertEqual(self.form.alignc_action.isChecked(), False)
+        self.assertEqual(self.form.alignr_action.isChecked(), False)
+        self.assertEqual(self.form.alignj_action.isChecked(), False)
+
+    def test_alignc_button(self):
+        # get the alignment buttons from the form
+        alignl_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignl_action)
+        alignc_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignc_action)
+        alignr_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignr_action)
+        alignj_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignj_action)
+        # left click the alignc button
+        QTest.mouseClick(alignc_button, Qt.LeftButton)
+        # assert that only the correct setting is enabled
+        self.assertEqual(self.form.alignl_action.isChecked(), False)
+        self.assertEqual(self.form.alignc_action.isChecked(), True)
+        self.assertEqual(self.form.alignr_action.isChecked(), False)
+        self.assertEqual(self.form.alignj_action.isChecked(), False)
+
+    def test_alignr_button(self):
+        # get the alignment buttons from the form
+        alignl_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignl_action)
+        alignc_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignc_action)
+        alignr_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignr_action)
+        alignj_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignj_action)
+        # left click the alignr button
+        QTest.mouseClick(alignr_button, Qt.LeftButton)
+        # assert that only the correct setting is enabled
+        self.assertEqual(self.form.alignl_action.isChecked(), False)
+        self.assertEqual(self.form.alignc_action.isChecked(), False)
+        self.assertEqual(self.form.alignr_action.isChecked(), True)
+        self.assertEqual(self.form.alignj_action.isChecked(), False)
+
+    def test_alignj_button(self):
+        # get the alignment buttons from the form
+        alignl_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignl_action)
+        alignc_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignc_action)
+        alignr_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignr_action)
+        alignj_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.alignj_action)
+        # left click the alignj button
+        QTest.mouseClick(alignj_button, Qt.LeftButton)
+        # assert that only the correct setting is enabled
+        self.assertEqual(self.form.alignl_action.isChecked(), False)
+        self.assertEqual(self.form.alignc_action.isChecked(), False)
+        self.assertEqual(self.form.alignr_action.isChecked(), False)
+        self.assertEqual(self.form.alignj_action.isChecked(), True)
+
 if __name__ == '__main__':
     unittest.main()
